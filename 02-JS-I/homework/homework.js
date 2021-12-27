2bbf7cbc54056519c3e22a1827b128119fd67e29
JoseRicci <riccijosesantiago@gmail.com>
2021-12-27 14:20:21 -0300

    finish 03-js-II homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -226,11 +226,10 @@ function esVocal(letra){
   //Escribe tu código aquí
   
   if(letra.length > 1){
-    return "Dato incorrecto"
-  }
+    return "Dato incorrecto"}
   if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
     return "Es vocal"
-  }
+}
   return "Dato incorrecto"
 
 }
